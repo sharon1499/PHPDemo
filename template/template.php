57c5219eb04a207e0a47360b7d2cf45d0edf63c7
Sharon Deloach <deloach_sharon@columbusstate.edu>
2019-09-05 22:17:02 +0000

    update17

--- template/template.php
+++ template/template.php
@@ -14,7 +14,7 @@
             <?php nav_menu(); ?>
         </nav>
     </header>
-    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhdpzEUPCxP3xqikVYImtcLUtjnRVXxRPCq6pL2ob3qgEIMNyG3w" height="200" width="700">
+    <img src="https://ec.europa.eu/jrc/sites/jrcsh/files/styles/normal-responsive/public/adobestock_21730836.jpeg?itok=uSbzpqGq" height="100%" width="100%">
     <article>
         <h2><?php page_title(); ?></h2>
         <?php page_content(); ?>
